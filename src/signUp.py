3577f72ad306c64da80b21b2457dc426f2df0f1c
Will Marceau <willfmarceau@gmail.com>
2025-02-13 22:24:05 -0800

    connected account creation front and back end

--- src/signUp.py
+++ src/signUp.py
@@ -4,6 +4,7 @@ import random
 from tkinter import messagebox
 import platform
 from landingPage import LandingPage
+from authentication_database import create_account
 
 class SignUp:
     def __init__(self, app, back, grid):
@@ -121,8 +122,11 @@ class SignUp:
         username_title = tk.Label(username_frame, text="Create Username")
         username_title.pack(pady=20)
 
-        username = tk.Entry(username_frame, width = 50)
-        username.pack(pady=20)
+        self.username = tk.Entry(username_frame, width = 50)
+        self.username.pack(pady=20)
+
+        # bind username to typing for valid submit checking
+        self.username.bind("<KeyRelease>", lambda event: self.check_inputs())
 
         pass_title = tk.Label(username_frame, text="Enter Password:")
         pass_title.pack(pady=20)
@@ -184,8 +188,8 @@ class SignUp:
         reset_bttn.pack(side=tk.LEFT, padx = 10, pady = 20)
 
         # create submit button
-        submit_bttn = tk.Button(bttn_frame, text = "Submit Account", command = lambda: self.submit())
-        submit_bttn.pack(side=tk.LEFT, padx = 10, pady = 20)
+        self.submit_bttn = tk.Button(bttn_frame, state = tk.DISABLED, text = "Submit Account", command = lambda: self.submit())
+        self.submit_bttn.pack(side=tk.LEFT, padx = 10, pady = 20)
 
         # set up scroll
         canvas.config(scrollregion=canvas.bbox("all"))
@@ -223,6 +227,7 @@ class SignUp:
                 self.check = True
         elif len(self.second_pass) == 6:
             if self.second_pass == self.entered_pass:
+                self.complete = True
                 messagebox.showinfo("Success", "Password Set, Submit to Create Account")
 
                 # disable all buttons
@@ -230,7 +235,7 @@ class SignUp:
                     bttn.config(image=self.faded_images[name], state=tk.DISABLED)
                 
                 # set done flag
-                self.complete = True
+                self.check_inputs()
 
             else:
                 messagebox.showerror("Error", "Passwords do not match, Try Again")
@@ -262,6 +267,11 @@ class SignUp:
     def submit(self):
         # this function will submit the account creation to database and bring user
         # back to the landing page
+
+        if (create_account(self.username.get(), self.entered_pass[0], self.entered_pass[1], self.entered_pass[2], self.entered_pass[3], self.entered_pass[4], self.entered_pass[5])):
+            messagebox.showinfo("Success", "Account Creation Successful, please Log In")
+        else:
+            messagebox.showerror("Error", "Account Creation Failed, Username Already Taken")
         
         for widget in self.app.winfo_children():
             widget.destroy()
@@ -284,3 +294,10 @@ class SignUp:
     def on_mousewheel_mac(self, event):
         # scroll for macOS
         self.canvas.yview_scroll(int(-1 * event.delta), "units")
+
+    def check_inputs(self):
+        # allow for submit if user enters required info
+        if self.username.get().strip() and self.complete:
+            self.submit_bttn.config(state=tk.NORMAL)
+        else:
+            self.submit_bttn.config(state=tk.DISABLED)
